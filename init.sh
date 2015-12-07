73b96fe5717dd95e09a34872f5d8fb90738b443d
dbawilson <yanginfor@qq.com>
2015-12-07 21:28:14 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,32 @@ chmod -R 777 /var/www
 service apache2 start
 su -c '/usr/bin/aria2c --conf-path=/cldata/aria2.conf' www-data
 /etc/init.d/ssh start
+if [ -f /.root_pw_set ]; then
+	echo "Root password already set!"
+	exit 0
+fi
+
+PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
+echo "=> Setting a ${_word} password to the root user"
+echo "root:$PASS" | chpasswd
+
+echo "=> Done!"
+if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
+    echo "=> Found authorized keys"
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+    IFS=$'\n'
+    arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
+    for x in $arr
+    do
+        x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
+        cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
+            echo "$x" >> /root/.ssh/authorized_keys
+        fi
+    done
+fi
